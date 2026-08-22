00b5123b81fbc2eebcabb0b0f0ae6bf9f4d9b4d4
fewizz <hazeevaidar@gmail.com>
2026-08-22 20:59:38 +0300

    SS traverser: fix cases when dir.z < 0.0 (rays are directed towards near plane)

--- assets/forgetmenot/shaders/lib/inc/raytrace.glsl
+++ assets/forgetmenot/shaders/lib/inc/raytrace.glsl
@@ -19,29 +19,25 @@ bool raytrace(inout vec3 pos, vec3 dir, int steps, sampler2D depths, out float d
         depth = texelFetch(depths, ivec2(pos.xy) >> level, level).r;
 
         vec3 advance; {
-            int cell_size = 1 << level; // 1, 4, 16, etc...
+            int cell_size = 1 << level;  // 1, 4, 16, etc...
             vec2 position_in_cell = mod(pos.xy * sign(dir.xy), cell_size);
             vec2 dists_to_axis = cell_size - position_in_cell;
             vec2 diagonal_dists = dists_to_axis / abs(dir.xy);
             float dist = min(diagonal_dists.x, diagonal_dists.y);
-            advance = max(dist, 0.01) * 1.01 * dir;
+            advance = max(dist, 0.001) * 1.001 * dir;
         }
 
-        if (pos.z + advance.z >= depth) {
-            pos += advance * max((depth - pos.z) / advance.z, 0.0); // go back
-            if (level > 0) {
-                level -= level_step;
-            }
-            else {
-                return true;
-            }
+        bool intersects =
+            (dir.z > 0.0) && (pos.z + advance.z >= depth) ||  // to the far plane
+            (dir.z < 0.0) && (pos.z >= depth);  // to the near plane
+
+        if (intersects) {
+            if (level == 0) return true;
+            advance *= max(sign(dir.z) * (depth - pos.z) / advance.z, 0.0);  // to the hit point
         }
-        else {
-            pos += advance;
-            if (level < last_level) {
-                level += level_step;
-            }
-        }
+
+        pos += advance;
+        level = clamp(level + (intersects ? -level_step : +level_step), 0, last_level);
     }
 
     return false;
